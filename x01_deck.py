942bcd14cf3b3409340de35d44ed7da1cdb26f5c
Diego Moy-Peche <90715994+Sebastianmaudee@users.noreply.github.com>
2022-01-26 14:35:55 -0800

    Update x01_deck.py

--- x01_deck.py
+++ x01_deck.py
@@ -6,20 +6,19 @@ the cards should be denoted with a number or A, J, Q, K, T (for ace, jack, queen
 as well as a suit
 """
 
+
 def createDeck():
   ranks = ['2','3','4','5','6','7','8','9','T','J','Q','K','A']
+  
   suits = ['C','D','H','S']
-  d = []
+  s = []
   for i in ranks:
     for u in suits:     
       deck = [i + u]
-      d = d + deck
-  return d
+      s = s + deck
+  return s
 
   
-
-#createDeck()
-
 
 
 
